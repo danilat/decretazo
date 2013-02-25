8ead283f0caa7599c7772e2de9926d4f746317bc
Rafael Ramos <rrasmo@gmail.com>
2013-02-25 02:13:11 +0100

    Mostrar últimos decretos en home

--- web/decretazo.py
+++ web/decretazo.py
@@ -1,16 +1,25 @@
+import os
+import json
 from flask import Flask
 from flask import render_template, url_for
+
+JSON_DIR = '../data/json/'
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    #TODO: fetch data from DB
-    #TODO: render template with data 
-    #return url_for('static', filename='style.css')
-    return render_template('home.html')
+    files = os.listdir(JSON_DIR)
+    files.sort()
+    files.reverse()
+    files = files[:5]
+    decrees = []
+    for f in files:
+        j = json.load(open(JSON_DIR + f))
+        decrees.append(j)
+    return render_template('home.html', decrees=decrees)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
