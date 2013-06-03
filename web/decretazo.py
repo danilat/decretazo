68e583fdb8d0c5e4c34ede7b3a8e62612e69cd9e
danilat <danilat83@gmail.com>
2013-06-04 01:17:54 +0200

    added show function

--- web/decretazo.py
+++ web/decretazo.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 from flask import Flask
 from flask import render_template, url_for
 
@@ -7,17 +8,31 @@ JSON_DIR = '../data/json/'
 
 app = Flask(__name__, static_url_path='')
 
-@app.route('/')
-def index():
+def read_files():
     files = os.listdir(JSON_DIR)
     files.sort()
     files.reverse()
+    return files
+
+@app.route('/')
+def index():
     decrees = []
-    for f in files:
+    years = {}
+    for f in read_files():
         j = json.load(open(JSON_DIR + f))
         decrees.append(j)
     return render_template('home.html', decrees=decrees)
 
+@app.route('/<year>')
+def show(year):
+    decrees = []
+    for f in read_files():
+        match = re.findall('BOE-A-\w+', f)[0]
+        file_year = match.split('BOE-A-')[1]
+        if year == file_year:
+            j = json.load(open(JSON_DIR + f))
+            decrees.append(j)
+    return render_template('show.html', decrees=decrees, year=year)
 
 if __name__ == '__main__':
     app.run(debug=True)
